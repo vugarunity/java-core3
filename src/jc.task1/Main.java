aa51c1e934257a973ea8be7187b6983725891d27
vugarunity <159554692+vugarunity@users.noreply.github.com>
2025-01-08 11:12:11 +0400

    Update Main.java

--- src/jc.task1/Main.java
+++ src/jc.task1/Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
 
-        // Примеры сравнения
-        System.out.println(employee.compareDates(2025, 12, 31, 2024, 1, 1));  // Положительное число
-        System.out.println(employee.compareDates(2024, 1, 1, 2025, 12, 31));  // Отрицательное число
-        System.out.println(employee.compareDates(2025, 12, 31, 2024, 12, 31)); // 0
+        System.out.println(employee.compareDates(2025, 12, 31, 2024, 1, 1)); 
+        System.out.println(employee.compareDates(2024, 1, 1, 2025, 12, 31));  
+        System.out.println(employee.compareDates(2025, 12, 31, 2024, 12, 31)); 
     }
 }
